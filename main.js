8dad27004f142cdbc156020a05e9cb1910dd97a2
hard2hannana <hard2hannana>
2025-11-16 12:44:43 -0500

    contact commit

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ galleryImages.forEach((img) => {
   image.alt = img.alt;
   gallery.appendChild(image);
 });
+const contactPanel = document.querySelector('.contact-content');
+  const closeBtn = document.getElementById('close-contact');
+  const form = document.getElementById('contact-form');
+
+
+  console.log("closeBtn is:", closeBtn);  
+  // Auto-open on load (mobile only)
+  window.addEventListener('load', () => {
+    if (window.innerWidth <= 768) {
+      contactPanel.classList.add('open');
+    }
+  });
+
+  closeBtn.addEventListener('click', () => {
+    contactPanel.classList.remove('open');
+  });
+ 
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    contactPanel.classList.remove('open');
+  });
+
